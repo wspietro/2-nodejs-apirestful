b09c51466e2b2dc8c371255e5fce367abce69fb4
Pietro Weg <pietro.sera@compasso.com.br>
2023-03-10 23:27:31 -0300

    :wrench: chore: aprendendo va

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,16 @@
 // arquivo de conexao com bd
 
+import 'dotenv/config' // export valores .env em process.env (global)
 import { knex as setupKnex, Knex } from 'knex'
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL not found')
+}
 
 export const knexConfig: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './db/app.db',
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
