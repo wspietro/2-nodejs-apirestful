5e705b63ea7976e1cf500406ff06a527c6247d6c
Pietro Weg <pietro.sera@compasso.com.br>
2023-03-13 15:02:29 -0300

    :sparkles: types: knex definition types

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,15 @@
+// reaproveitar todos os tipos existentes dentro de knex
+// eslint-disable-next-line
+import { knex } from 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    transactions: {
+      id: string
+      title: string
+      amount: number
+      created_at: string
+      session_id: string?
+    }
+  }
+}
