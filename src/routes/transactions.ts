f2972cc35a726b6b15bd781bb4f3d6c264577eb0
Pietro Weg <pietro.sera@compasso.com.br>
2023-03-13 23:48:46 -0300

    :construction: feat: contexto global plugin preHandler

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -7,6 +7,10 @@ import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 // fastify plugin
 // route prefix: transactions
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.addHook('preHandler', async (request, reply) => {
+    console.log(`[${request.method} ${request.url}]`)
+  })
+
   app.get(
     '/',
     {
