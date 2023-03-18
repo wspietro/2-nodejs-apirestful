a8ebb186089345d4a1f69eff9225c334080e16b5
Pietro Weg <pietro.sera@compasso.com.br>
2023-03-18 15:13:11 -0300

    :test_tube: test: rotas get id e summary

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -60,4 +60,68 @@ describe('Traansactions routes', () => {
       }),
     ])
   })
+
+  it('should be able to get a specific transaction', async () => {
+    const transactionMock = {
+      title: 'New Transaction',
+      amount: 5000,
+      type: 'credit',
+    }
+
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send(transactionMock)
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    const transactionId = listTransactionsResponse.body.transactions[0].id
+
+    const getTransactionResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(getTransactionResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        id: transactionId,
+        title: 'New Transaction',
+        amount: 5000,
+      }),
+    )
+  })
+
+  it('should be able to get summary of transctions amount', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Credit Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({
+        title: 'Debit Transaction',
+        amount: 2000,
+        type: 'debit',
+      })
+
+    const summaryAmountResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(summaryAmountResponse.body.summary).toEqual({
+      amount: 3000,
+    })
+  })
 })
