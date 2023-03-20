f378d7ecad6a8cd6f67f084f4fefecc1fbd97026
Pietro Weg <pietro.sera@compasso.com.br>
2023-03-19 22:00:37 -0300

    :bug: fix: corrigindo migration

--- db/migrations/20230311012541_create-transactions.ts
+++ db/migrations/20230311012541_create-transactions.ts
@@ -6,7 +6,7 @@ export async function up(knex: Knex): Promise<void> {
     table.uuid('id').primary()
     table.text('title').notNullable()
     table.decimal('amount', 10, 2).notNullable()
-    table.timestamp('created_at').defaultTo(knex.fn.now).notNullable() // evitar erro na troca do banco knex.fn
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable() // evitar erro na troca do banco knex.fn
   })
 }
 
